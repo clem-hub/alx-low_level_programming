9166afbf782b5571dc3cb55f3881a9d68044dd73
clem-hub <clemenciagwaya300@gmail.com>
2023-02-21 18:00:52 -0800

    done 1-3

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase alphabets
+ *
+ * Return: void
+ */
+void print_alphabet(void);
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c+=)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase alphabets 10x
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void);
+{
+        char c;
+	int i;
+
+	for (i = 0; 1 < 10; 1 ++)
+	{
+		for (c = 'a'; c <= 'z'; c+=)
+		{
+                putchar(c);
+		}
+		putchar('\n');
+        }
+}
