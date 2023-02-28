59a2a369ecbb14afde36d1ae22a8606b92f1e6d3
clem-hub <clemenciagwaya300@gmail.com>
2023-02-28 18:30:53 +0100

    corrected 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,11 +5,11 @@
  * and updates the value it points to to 98
  * @n: an integer
  *
- * Return: 98
+ * Return: ALways void
  */
 void reset_to_98(int *n)
 {
 	*n = 98;
 
-	return (n);
+	return (void);
 }
