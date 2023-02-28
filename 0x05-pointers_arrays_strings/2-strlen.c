5a8e40c96a04212fbdc2ecdc6d344cea639904c4
clem-hub <clemenciagwaya300@gmail.com>
2023-02-28 21:21:29 +0100

    done 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the length of a string
+ * @s: string
+ *
+ * Return: lenth of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while ( s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
