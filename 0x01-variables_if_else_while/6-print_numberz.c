53443dda4085e1dcb63291f9e103facd0ef9e5c4
clem-hub <clemenciagwaya300@gmail.com>
2023-07-14 20:00:58 -0700

    corrected 0,1,3,6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 /**
  * main - prints all single digit numbers of base 10 starting from 0
@@ -15,8 +16,8 @@ int main(void)
 
 	for (i = 48; i <= 57; i++)
 	{
-		putchar (i);
+		putchar(i);
 	}
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
