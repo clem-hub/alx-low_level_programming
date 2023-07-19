54a0a4b35302f680e5b892c3a184c0fe47656db5
clem-hub <clemenciagwaya300@gmail.com>
2023-07-19 03:50:39 +0100

    task 0-5

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ * 
+ * Return: Always 0
+ */
+int main(void)
+{
+	char txt[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(txt[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
